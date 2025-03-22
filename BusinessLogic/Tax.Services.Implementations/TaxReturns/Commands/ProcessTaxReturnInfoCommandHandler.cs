81654dea57c1f9bac3749c8ed4a814ea2bffd034
Bernardo T Barata <bernardo.barata@smartmed.world>
2025-03-22 19:22:08 +0000

    feat: add computations to handler

--- BusinessLogic/Tax.Services.Implementations/TaxReturns/Commands/ProcessTaxReturnInfoCommandHandler.cs
+++ BusinessLogic/Tax.Services.Implementations/TaxReturns/Commands/ProcessTaxReturnInfoCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Tax.Entities.TaxReturns;
+﻿using System.Reflection;
+using Tax.Entities.TaxReturns;
 using Tax.Services.Implementations.Common;
 using Tax.Services.TaxReturns.Commands;
 
@@ -6,15 +7,88 @@ namespace Tax.Services.Implementations.TaxReturns.Commands
 {
     public class ProcessTaxReturnInfoCommandHandler : ICommandHandler<ProcessTaxReturnInfoCommand, TaxReturnInfo>
     {
+        private ProcessTaxReturnInfoCommand _command;
+
         public async Task<TaxReturnInfo> HandleAsync(ProcessTaxReturnInfoCommand command)
         {
-            return await Task.FromResult(new TaxReturnInfo
+            _command = command;
+            TaxReturnInfo processedTaxReturnInfo = ComputeTaxAmounts(_command.TaxReturnInfo);
+            return await Task.FromResult(processedTaxReturnInfo);
+        }
+
+        private TaxReturnInfo ComputeTaxAmounts(TaxReturnInfo taxReturnInfo)
+        {
+            string validAmountPropertyName = taxReturnInfo.GetType()
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.PropertyType == typeof(decimal?) && p.GetValue(taxReturnInfo) != null)
+                .Select(p => p.Name)
+                .First();
+            TaxReturnInfo result = new TaxReturnInfo();
+            switch (validAmountPropertyName)
             {
-                NetValue = 5,
-                GrossValue = 10,
-                VATValue = 11,
-                AustrianVATRate = VATRate.VAT13Percent
-            });
+                case nameof(TaxReturnInfo.NetValue):
+                    ProcessTaxInfoBasedOnNetValue();
+                    break;
+                case nameof(TaxReturnInfo.GrossValue):
+                    ProcessTaxInfoBasedOnGrossValue();
+                    break;
+                case nameof(TaxReturnInfo.VATValue):
+                    ProcessTaxInfoBasedOnVATValue();
+                    break;
+            }
+            return result;
+        }
+
+        private TaxReturnInfo ProcessTaxInfoBasedOnNetValue()
+        {
+            decimal? netValue = _command.TaxReturnInfo.NetValue;
+            decimal vatRate = GetVATRate();
+            decimal? vatValue = netValue / vatRate;
+            return CreateTaxReturnInfo(
+                grossValue: netValue + vatValue,
+                netValue,
+                vatValue);
+        }
+
+        private decimal GetVATRate()
+        {
+            return ((int)_command.TaxReturnInfo.AustrianVATRate) * 0.01m;
+        }
+
+        private TaxReturnInfo CreateTaxReturnInfo(
+            decimal? grossValue,
+            decimal? netValue,
+            decimal? vatValue)
+        {
+            return new TaxReturnInfo
+            {
+                GrossValue = grossValue,
+                NetValue = netValue,
+                VATValue = vatValue,
+                AustrianVATRate = _command.TaxReturnInfo.AustrianVATRate
+            };
+        }
+
+        private TaxReturnInfo ProcessTaxInfoBasedOnGrossValue()
+        {
+            decimal? grossValue = _command.TaxReturnInfo.GrossValue;
+            decimal vatRate = GetVATRate();
+            decimal? vatValue = grossValue * vatRate;
+            return CreateTaxReturnInfo(
+                grossValue,
+                netValue: grossValue - vatValue,
+                vatValue);
+        }
+
+        private TaxReturnInfo ProcessTaxInfoBasedOnVATValue()
+        {
+            decimal? vatValue = _command.TaxReturnInfo.VATValue;
+            decimal vatRate = GetVATRate();
+            decimal? grossValue = vatValue / vatRate;
+            return CreateTaxReturnInfo(
+                grossValue,
+                netValue: grossValue - vatValue,
+                vatValue);
         }
     }
 }
